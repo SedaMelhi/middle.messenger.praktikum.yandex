e4bd5e2ec1638f63d0ad0883a51daaea5255a1c7
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-12-25 19:29:53 +0300

    chore: set up Vite build process

---
+++ src/components/index.ts
@@ -0,0 +1,7 @@
+export default {};
+
+// export { Button } from './button'
+// export { Input } from './input'
+// export { FormAuth } from './form-auth'
+// export { ListCat } from './list-cats'
+// export { CatCard } from './cat-card'
