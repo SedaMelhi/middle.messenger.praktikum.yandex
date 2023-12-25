31ea2dc82f513471fc5c4136b4ebfad69d2c3931
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2023-12-25 19:34:12 +0300

    Revert "chore: set up Vite build process"
    This reverts commit e4bd5e2ec1638f63d0ad0883a51daaea5255a1c7.

--- src/components/index.ts
+++
@@ -1,7 +0,0 @@
-export default {};
-
-// export { Button } from './button'
-// export { Input } from './input'
-// export { FormAuth } from './form-auth'
-// export { ListCat } from './list-cats'
-// export { CatCard } from './cat-card'
