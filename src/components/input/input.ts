11c7d27e6f37dc15685786bd884b656efdd13c70
SedaMelhi <31256499+SedaMelhi@users.noreply.github.com>
2024-03-05 23:29:03 +0300

    .

--- src/components/input/input.ts
+++ src/components/input/input.ts
@@ -1,1 +1,34 @@
-export { default as Input } from './input.hbs?raw';
+import Block from '../../core/Block';
+import { IProps, RefType } from '../../core/Block';
+
+export interface InputProps extends IProps {
+  ref: string;
+  name: string;
+  value: string;
+  type: string;
+  classes: string;
+  placeholder: string;
+  onChange: (e: Event) => void;
+}
+export class Input extends Block<InputProps, RefType> {
+  constructor(props: InputProps) {
+    super({
+      ...props,
+      events: {
+        change: props.onChange,
+      },
+    });
+  }
+
+  protected render(): string {
+    const { classes, placeholder, name, type, value } = this.props;
+    return `<input 
+        class="${classes}" 
+        placeholder="${placeholder}" 
+        value="${value}" 
+        name="${name}" 
+        type="${type}" 
+        value="${value}"
+    />`;
+  }
+}
